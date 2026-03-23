339aee4ed024fe5a6430b12a4bf189ac287c9f35
Trisha81 <trishaj961@gmail.com>
2026-03-23 09:16:52 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import ThemeToggle from "./components/ThemeToggle";
-
-export default function App() {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ThemeToggle />
-    </div>
-  );
-}
